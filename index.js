d26e3a19464f1b2ff3cc9a2cd5475a95559b5f1c
Niti Nrw <niti.patel@flatironschool.com>
2016-11-17 09:43:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much.";
+export default class Panel extends React.Component {
+  render() {
+    return (
+      <div className="panel">
+        <div className="panel-header">{this.props.title}</div>
+        <div className="panel-body">{this.props.children}</div>
+      </div>
+    );
+  }
+}
